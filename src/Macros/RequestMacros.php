91f8502c43a548a3c6ef9449fffc83dd325e8eb7
Dmitry Borzyonok <dima.matters@gmail.com>
2020-06-14 02:49:58 +0300

    Revert request

--- src/Macros/RequestMacros.php
+++ src/Macros/RequestMacros.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace McMatters\Helpers\Macros;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Request as RequestFacade;
 use Illuminate\Support\Str;
 
 use function in_array;
@@ -18,7 +19,7 @@ use const true;
  */
 class RequestMacros extends AbstractMacroable
 {
-     /**
+    /**
      * @return string
      */
     public static function getClass(): string
@@ -27,10 +28,15 @@ class RequestMacros extends AbstractMacroable
     }
 
     /**
+     * @param \Illuminate\Http\Request|null $request
+     *
      * @return bool
      */
-    public function registerIsUpdateMethod(): bool
+    public function registerIsUpdateMethod(Request $request = null): bool
     {
-        return in_array(Str::lower($this->method()), ['put', 'patch'], true);
+        /** @var \Illuminate\Http\Request $request */
+        $request = $request ?: RequestFacade::instance();
+
+        return in_array(Str::lower($request->method()), ['put', 'patch'], true);
     }
 }
